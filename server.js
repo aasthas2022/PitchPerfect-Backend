e71f11ff4bfa4597c5cfe7c569d09ebd4c41b9cc
AasthaPersonalGithub <aasthas2022@gmail.com>
2024-06-23 19:13:01 -0400

    Prepare project for Netlify deployment

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 /**
- * src/server.js
+ * server.js
  * 
  * This is the main entry point for the server application. It sets up the Express server, 
  * loads environment variables, and mounts the API routes. The server listens on a specified 
@@ -8,18 +8,19 @@
 
 require('dotenv').config();
 const express = require('express');
-const cors = require('cors'); 
+const serverless = require('serverless-http');
+const cors = require('cors');
 const routes = require('./src/routes');
 
 const app = express();
+const router = express.Router();
+
 const PORT = process.env.PORT || 3000;
 
-app.use(cors()); 
+app.use(cors());
 app.use(express.json());
 app.use('/api', routes);
 
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-});
+router.use('/.netlify/functions/server', app);
 
-module.exports = app;
+module.exports.handler = serverless(router);
